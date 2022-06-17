c847062dc22d21816ef8986b2da4f94995c01dce
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-06-18 02:11:07 +0430

    add basename "varzeshgah" to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { FontawesomeProvider } from "utils/adapters";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="varzeshgah">
       <FontawesomeProvider>
         <App />
       </FontawesomeProvider>
