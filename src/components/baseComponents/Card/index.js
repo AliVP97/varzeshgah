d3eb0db9fa46ad46cd3f13948a8add593b791e1b
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-06-21 22:59:04 +0430

    📦 refactor/(Card)  Enhance Card component

--- src/components/baseComponents/Card/index.js
+++ src/components/baseComponents/Card/index.js
@@ -1,5 +1,5 @@
 const Card = ({ className, style, children }) => (
-  <div className={`rounded-md p-3 m-4 ${className}`} style={style}>
+  <div className={`rounded-2xl p-3 m-4 ${className}`} style={style}>
     {children}
   </div>
 );
