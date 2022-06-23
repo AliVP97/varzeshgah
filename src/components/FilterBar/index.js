33ec244be80db6744950239ccaedf87b6f7beb79
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-06-23 17:43:37 +0430

    📦 refactor/(FilterBar)  Avoid unnecessary reapeating
    + Fix divider

--- src/components/FilterBar/index.js
+++ src/components/FilterBar/index.js
@@ -1,18 +1,25 @@
-const FilterBar = () => {
+const buttons = [
+  { title: "نقشه", iconClass: "fa-solid fa-map" },
+  { title: "فیلتر", iconClass: "fa-solid fa-filter" },
+  { title: "ترتیب", iconClass: "fa-solid fa-bars-sort" },
+];
+
+const FilterBar = ({ className }) => {
   return (
-    <div className="flex justify-between text-slate-500 font-bold bg-white m-4 px-6 py-5 rounded-2xl shadow-xl divide-x">
-      <div className="inline-flex items-center gap-x-2">
-        <i className="fa-solid fa-map"></i>
-        <p>نقشه</p>
-      </div>
-      <div className="inline-flex items-center gap-x-2">
-        <i className="fa-solid fa-filter"></i>
-        <p>فیلتر</p>
-      </div>
-      <div className="inline-flex items-center gap-x-2">
-        <i className="fa-solid fa-bars-sort"></i>
-        <p>ترتیب</p>
-      </div>
+    <div
+      className={`flex justify-between text-slate-500 font-bold bg-white m-4 py-5 rounded-2xl shadow-xl divide-x divide-x-reverse${
+        className ? ` ${className}` : ""
+      }`}
+    >
+      {buttons.map(({ title, iconClass }) => (
+        <div
+          key={title}
+          className="w-1/3 inline-flex justify-center items-center gap-x-2"
+        >
+          <i className={iconClass}></i>
+          <p>{title}</p>
+        </div>
+      ))}
     </div>
   );
 };
