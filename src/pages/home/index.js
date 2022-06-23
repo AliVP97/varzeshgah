28f7a11536cc12d77794d022744d6c8ce08f9b1e
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-06-23 17:44:55 +0430

    🐛 fix/(home)  Fix scroll problem
    + Add compactMode when scrolling

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   const [data, setData] = useState();
+  const [compactMode, setCompactMode] = useState(false);
 
   useEffect(() => {
     fetch("data.json", {
@@ -15,17 +16,44 @@ const Home = () => {
       .then((data) => setData(data));
   }, []);
 
+  const scrollHandler = ({ target: { scrollTop } }) => {
+    if (scrollTop > 80) {
+      setCompactMode(true);
+    } else {
+      setCompactMode(false);
+    }
+  };
+
   return (
-    <div className="container bg-slate-900">
-      <div className="h-[8rem] mx-8 relative flex justify-center items-center">
+    <div className="h-screen bg-slate-900">
+      <div
+        style={{ height: compactMode ? 25 : 80 }}
+        className={`relative transition-[height] py-5 mx-8 flex justify-center items-center`}
+      >
         <i className="fa-solid fa-arrow-right absolute right-0 text-yellow-400 text-xl"></i>
-        <div className="flex gap-x-4">
-          <i className="fa-solid fa-location-dot text-yellow-400 text-2xl"></i>
-          <p className="text-3xl text-slate-50">گرگان</p>
+        <div className="flex items-center gap-x-4">
+          <i
+            style={{ fontSize: compactMode ? "1.25rem" : "1.5rem" }}
+            className="fa-solid fa-location-dot pb-1 text-yellow-400 text-2xl"
+          ></i>
+          <p
+            style={{ fontSize: compactMode ? "1.5rem" : "1.875rem" }}
+            className="transition-[font-size] text-slate-50"
+          >
+            گرگان
+          </p>
         </div>
       </div>
-      <div className="container bg-slate-50 py-3 rounded-3xl">
-        <FilterBar />
+      <div
+        style={{
+          height: compactMode
+            ? window.innerHeight - 25 - 15
+            : window.innerHeight - 80,
+        }}
+        className="overflow-y-auto transition-[height] bg-slate-50 py-3 rounded-t-3xl"
+        onScroll={scrollHandler}
+      >
+        <FilterBar className="sticky top-0 z-10" />
         <div className="px-4 space-y-4">
           {data?.map((product) => (
             <Product key={product.name} data={product} />
