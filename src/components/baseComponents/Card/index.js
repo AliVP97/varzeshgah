f7c0331c2fdb1e98e57dda4969e63a829cd0face
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-06-24 05:29:17 +0430

    📦 refactor/(Card)  Add props drilling option

--- src/components/baseComponents/Card/index.js
+++ src/components/baseComponents/Card/index.js
@@ -1,5 +1,5 @@
-const Card = ({ className, style, children }) => (
-  <div className={`rounded-2xl p-3 ${className}`} style={style}>
+const Card = ({ className, children, ...props }) => (
+  <div className={`rounded-2xl p-3 ${className}`} {...props}>
     {children}
   </div>
 );
