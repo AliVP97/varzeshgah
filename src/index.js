a0243ae8f8ef135f21834cf884d6c30175083555
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-06-26 03:28:48 +0430

    🛠 build/(.env)  Setup .env variables

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="varzeshgah">
+    <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
