49c12f1239dec54b34dfe78f5350d9a683b815de
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-06-26 03:30:40 +0430

    ✨ feat/(routes)  Add authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,46 @@
-import { Routes as RouterRoutes, Route } from "react-router-dom";
-import { DataEntry, Home, Place } from "pages";
+import { Routes as RouterRoutes, Route, Navigate } from "react-router-dom";
+import { DataEntry, Home, Login, Place } from "pages";
 
-const Routes = () => (
-  <RouterRoutes>
-    <Route path="/">
-      <Route index element={<Home />} />
-      <Route path=":id" element={<Place />} />
-      <Route path="dataEntry" element={<DataEntry />} />
-    </Route>
-  </RouterRoutes>
-);
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "services";
+import { useState } from "react";
+
+const Routes = () => {
+  const [isAuth, setIsAuth] = useState(
+    localStorage.getItem("JWT") || auth.currentUser
+  );
+
+  onAuthStateChanged(auth, function (user) {
+    if (user) {
+      // User is signed in.
+      setIsAuth(true);
+      const {
+        stsTokenManager: { accessToken, refreshToken, expirationTime },
+      } = user;
+
+      localStorage.setItem("JWT", accessToken);
+      localStorage.setItem("rJWT", refreshToken);
+      localStorage.setItem("expJWT", expirationTime);
+    } else {
+      // No user is signed in.
+      setIsAuth(false);
+      localStorage.clear();
+    }
+  });
+
+  return (
+    <RouterRoutes>
+      <Route path="/" element={isAuth ? undefined : <Navigate to={"/login"} />}>
+        <Route index element={<Home />} />
+        <Route path=":id" element={<Place />} />
+        <Route path="dataEntry" element={<DataEntry />} />
+      </Route>
+      <Route
+        path="/login"
+        element={isAuth ? <Navigate to={"/"} /> : <Login />}
+      />
+    </RouterRoutes>
+  );
+};
 
 export default Routes;
